69b79ee84972acc69f548d0255ce938ace865e74
Georgy Guminov <gogagum@gmail.com>
2022-12-14 19:33:37 +0300

    Static dictionary tests[2].

--- test.cpp
+++ test.cpp
@@ -419,8 +419,58 @@ TEST(StaticDictionary, Ord2Bytes2) {
 
     auto dict = ga::dict::StaticDictionary<ga::BytesSymbol<2>>(freq.begin(), freq.end());
 
-    auto word1Bytes = std::array<std::byte, 2>{std::byte{0}, std::byte{41}};
+    auto wordBytes = std::array<std::byte, 2>{std::byte{0}, std::byte{41}};
+    auto word = ga::BytesSymbol<2>(wordBytes.data());
+
+    EXPECT_EQ(dict.getWord(36), word);
+}
+
+//----------------------------------------------------------------------------//
+TEST(StaticDictionary, CumulativeNumFoundLow) {
+    auto freq = std::vector<std::uint64_t>(256 * 256, 0);
+    std::fill(freq.begin() + 42, freq.begin() + 112, 37);
+    std::fill(freq.begin() + 112, freq.end(), 42);
+
+    auto dict = ga::dict::StaticDictionary<ga::BytesSymbol<2>>(freq.begin(), freq.end());
+
+    auto word1Bytes = std::array<std::byte, 2>{std::byte{0}, std::byte{112}};
     auto word1 = ga::BytesSymbol<2>(word1Bytes.data());
 
-    EXPECT_EQ(dict.getWord(36), word1);
+    auto word2Bytes = std::array<std::byte, 2>{std::byte{0}, std::byte{113}};
+    auto word2 = ga::BytesSymbol<2>(word2Bytes.data());
+
+    EXPECT_EQ(dict.getLowerCumulativeNumFound(word1), 37);
+    EXPECT_EQ(dict.getLowerCumulativeNumFound(word2), 42);
+}
+
+//----------------------------------------------------------------------------//
+TEST(StaticDictionary, CumulativeNumFoundZero) {
+    auto freq = std::vector<std::uint64_t>(256, 0);
+    std::fill(freq.begin() + 42, freq.begin() + 112, 37);
+    std::fill(freq.begin() + 112, freq.end(), 42);
+
+    auto dict = ga::dict::StaticDictionary<ga::BytesSymbol<1>>(freq.begin(), freq.end());
+
+    auto wordBytes = std::array<std::byte, 1>{std::byte{0}};
+    auto word = ga::BytesSymbol<1>(wordBytes.data());
+
+    EXPECT_EQ(dict.getLowerCumulativeNumFound(word), 0);
+}
+
+//----------------------------------------------------------------------------//
+TEST(StaticDictionary, CumulativeNumFoundHigh) {
+    auto freq = std::vector<std::uint64_t>(256 * 256, 0);
+    std::fill(freq.begin() + 42, freq.begin() + 112, 37);
+    std::fill(freq.begin() + 112, freq.end(), 42);
+
+    auto dict = ga::dict::StaticDictionary<ga::BytesSymbol<2>>(freq.begin(), freq.end());
+
+    auto word1Bytes = std::array<std::byte, 2>{std::byte{0}, std::byte{111}};
+    auto word1 = ga::BytesSymbol<2>(word1Bytes.data());
+
+    auto word2Bytes = std::array<std::byte, 2>{std::byte{0}, std::byte{112}};
+    auto word2 = ga::BytesSymbol<2>(word2Bytes.data());
+
+    EXPECT_EQ(dict.getHigherCumulativeNumFound(word1), 37);
+    EXPECT_EQ(dict.getHigherCumulativeNumFound(word2), 42);
 }
